ae60a8e8e6ca0f057ba0ee9756d570f137492794
Hunter Peress <772917+h-unterp@users.noreply.github.com>
2023-03-28 10:53:39 -0400

    create example, compiles

---
+++ example/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_font_finder/flutter_font_finder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: ref.watch(currentFontProvider),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const FontSel(),
+          toolbarHeight: 125,
+        ),
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        )));
+  }
+}
--- lib/flutter_font_finder.dart
+++ lib/flutter_font_finder.dart
@@ -1,1 +1,4 @@
 library flutter_font_finder;
+
+export 'src/font_chooser.dart';
+export 'src/font_provider.dart';
